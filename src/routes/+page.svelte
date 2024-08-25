291f882c6434e433334e37421657a1c032976ba3
hitochito <hitochito0@gmail.com>
2024-08-25 14:13:49 +0600

    test icon component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import Icon from '$lib/components/ui/icons/Icon.svelte';
+</script>
+
+<Icon icon="menu" />
