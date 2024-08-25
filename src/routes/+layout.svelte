844f2007500cbb8c50db55079ed867d37917324b
hitochito <hitochito0@gmail.com>
2024-08-25 20:19:17 +0600

    add navbar to layout.svelte instead

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,8 +2,10 @@
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 	import '@fontsource-variable/grandstander'; // weights 100-900;
+	import Navbar from '$lib/components/navigation/Navbar.svelte';
 </script>
 
 <ModeWatcher />
+<Navbar />
 
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,50 @@
 <script>
-	import Icon from '$lib/components/ui/icons/Icon.svelte';
+	import Navbar from '$lib/components/navigation/Navbar.svelte';
 </script>
 
-<Icon icon="menu" />
+<main class="container mx-auto">
+	<p class="text-justify">
+		startinggggggggggggg Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit blanditiis
+		officiis eaque tenetur ut hic laudantium pariatur praesentium, veritatis sint earum voluptates
+		repellendus nulla inventore eum nesciunt odit aliquam natus dolore. Iusto iste corrupti ea
+		voluptates tenetur quia repellendus, repellat, iure facilis sed reprehenderit ex rerum quisquam
+		ipsum accusamus doloremque vel cumque provident eius ratione. Repudiandae assumenda
+		exercitationem illo eos adipisci enim, asperiores ex facilis voluptatibus, ab ipsum soluta
+		laudantium ut reiciendis neque delectus minus quisquam perspiciatis a, ipsam quas! Non omnis,
+		neque nulla alias atque sapiente deserunt consequatur id nihil. Commodi debitis laborum odio
+		amet, consectetur, tempora nihil sapiente voluptas deleniti nemo officiis earum, ipsa
+		perferendis a fugit id quas atque exercitationem alias dolorem itaque reiciendis. Dignissimos
+		obcaecati beatae, ratione maiores odit vel necessitatibus nostrum excepturi minima, consequatur
+		aspernatur voluptates quis voluptatibus. Officia pariatur, consequatur enim fugit nihil
+		quibusdam amet explicabo illum repudiandae maiores atque incidunt rerum eos quisquam ut? Minima
+		unde porro aut, quas commodi quis autem corrupti ea molestias at totam natus deserunt
+		consequatur beatae maiores! Neque dolore ipsam expedita. Voluptatem aut delectus consequuntur
+		aspernatur veritatis porro autem recusandae officia, laborum excepturi voluptate. Aperiam
+		exercitationem, perferendis explicabo quas iste consequuntur minus voluptatum vel facere
+		corrupti excepturi sequi totam ullam omnis perspiciatis ipsa tenetur laboriosam. Ipsam doloribus
+		minus natus suscipit officia voluptates dolore explicabo aperiam inventore corporis vel,
+		eligendi pariatur totam repudiandae error excepturi libero soluta ipsum perferendis, sint
+		laudantium recusandae? Quibusdam inventore ipsa autem minus, iste eaque adipisci tempora
+		praesentium. Alias cupiditate voluptatibus laudantium mollitia quidem obcaecati pariatur odio
+		accusamus? Minus sed perspiciatis quos, pariatur dolor omnis aut corporis magnam assumenda
+		atque. Sed repellendus voluptatibus provident blanditiis eius, suscipit eos tenetur molestias,
+		aliquam explicabo est non quia minima ipsam rerum nihil a quod! Perferendis corrupti ut nisi
+		voluptatum, quae quod. Dolore fugit omnis quisquam suscipit ullam nemo vitae delectus quae
+		explicabo asperiores velit cupiditate, distinctio repellat sapiente natus nostrum consequuntur
+		laborum sit tempora earum rem! Aut eveniet architecto minima, odit sunt nulla reiciendis quae,
+		sint accusantium temporibus excepturi itaque consectetur facere explicabo, nobis maiores
+		voluptatem magni quasi aliquid libero autem et. Quo velit commodi alias deleniti eaque fugit,
+		esse excepturi vero ullam, a aspernatur voluptatum dolor adipisci, cupiditate accusantium
+		impedit iusto? Nostrum dolorum aliquam quam delectus expedita animi facilis inventore numquam,
+		eos qui dolorem reiciendis perspiciatis hic, quos, dicta reprehenderit. Explicabo distinctio
+		voluptate incidunt veritatis? Cum repellat iure obcaecati a in eos saepe soluta distinctio id
+		vitae nobis ipsa, tempora nostrum nihil ex, odio impedit aperiam et provident neque ab. Veniam
+		voluptatibus similique ipsam quasi! Fuga a quam itaque amet possimus soluta neque laboriosam,
+		repellendus, quisquam eaque quis. Delectus quo dolore inventore molestias neque, consequatur
+		eligendi ab harum tempore labore velit repellat libero. Odit qui repellat facere? Omnis placeat
+		qui eveniet, perspiciatis dignissimos, nobis et exercitationem rerum illo repellendus non itaque
+		maiores ea maxime fugit delectus laboriosam temporibus voluptates nulla. Sit architecto atque
+		suscipit quod delectus culpa quibusdam asperiores iusto totam aliquid, aut eos saepe eveniet,
+		maxime quaerat quo eum distinctio obcaecati, ea tempora! Reprehenderit, nam inventore! ENDINGGGG
+	</p>
+</main>
