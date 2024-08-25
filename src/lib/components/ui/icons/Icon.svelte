f016771f22b38e834809b6149aa3bf0dbe1ed1bf
hitochito <hitochito0@gmail.com>
2024-08-25 14:08:25 +0600

    add iconify icon component

---
+++ src/lib/components/ui/icons/Icon.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import 'iconify-icon';
+
+	const icons = {
+		menu: 'ic:sharp-menu'
+		// Add more icons here as needed
+	} as const;
+	type IconName = keyof typeof icons;
+
+	export let icon: IconName;
+	export let size = '1em';
+	export let color = 'currentColor';
+</script>
+
+<svelte:element this={'iconify-icon'} icon={icons[icon]} {size} {color} {...$$restProps} />
