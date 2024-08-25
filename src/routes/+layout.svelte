0607b4e0ad2064cecd3fc5df3c124806be15bf72
hitochito <hitochito0@gmail.com>
2024-08-25 20:36:35 +0600

    add view-transitions api

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,23 @@
 <script>
-	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
-	import '@fontsource-variable/grandstander'; // weights 100-900;
+	import { ModeWatcher } from 'mode-watcher';
+	import { onNavigate } from '$app/navigation';
 	import Navbar from '$lib/components/navigation/Navbar.svelte';
+	import '@fontsource-variable/grandstander'; // weights 100-900;
+
+	// VIEW TRANSITIONS API
+	onNavigate((navigation) => {
+		// @ts-ignore <-- This is a private API so we need to ignore the TS error
+		if (!document.startViewTransition) return;
+
+		return new Promise((resolve) => {
+			// @ts-ignore <-- This is a private API so we need to ignore the TS error
+			document.startViewTransition(async () => {
+				resolve();
+				await navigation.complete;
+			});
+		});
+	});
 </script>
 
 <ModeWatcher />
