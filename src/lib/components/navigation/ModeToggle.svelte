e33bc2b191cdeec4b0b1654b4d668241827e18c8
hitochito <hitochito0@gmail.com>
2024-08-25 16:15:10 +0600

    update modetoggle component

--- src/lib/components/navigation/ModeToggle.svelte
+++ src/lib/components/navigation/ModeToggle.svelte
@@ -1,17 +1,14 @@
 <script lang="ts">
-	import { toggleMode } from 'mode-watcher';
+	import { toggleMode, mode } from 'mode-watcher';
 	import { Button } from '$lib/components/ui/button/index.js';
 	import Icon from '$lib/components/ui/icons/Icon.svelte';
 </script>
 
-<Button onclick={toggleMode} variant="outline" size="icon">
-	<Icon
-		icon="sun"
-		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-	/>
-	<Icon
-		icon="moon"
-		class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-	/>
+<Button on:click={toggleMode} variant="ghost" class="ok">
+	{#if $mode === 'light'}
+		<Icon icon="sun" class="text-lg" />
+	{:else}
+		<Icon icon="moon" class="text-lg" />
+	{/if}
 	<span class="sr-only">Toggle theme</span>
 </Button>
