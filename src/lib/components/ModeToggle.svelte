872f3e9f9b3955d968b90afb0bec0da1cc893b36
hitochito <hitochito0@gmail.com>
2024-08-26 21:18:24 +0600

    fix darkmode toggle to bottom right

--- src/lib/components/ModeToggle.svelte
+++ src/lib/components/ModeToggle.svelte
@@ -5,9 +5,15 @@
 
 <button on:click={toggleMode}>
 	{#if $mode === 'light'}
-		<Iconify icon="sun" />
+		<Iconify class="text-lg" icon="sun" />
 	{:else}
-		<Iconify icon="moon" />
+		<Iconify class="text-lg" icon="moon" />
 	{/if}
 	<span class="sr-only">Toggle theme</span>
 </button>
+
+<style lang="postcss">
+	button {
+		@apply fixed bottom-5 right-5 rounded px-2 py-1 bg-muted text-foreground;
+	}
+</style>
