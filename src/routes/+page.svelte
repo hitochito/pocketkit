4b3e6a98dbedb1efa736e3cdc80c1279003fe871
hitochito <hitochito0@gmail.com>
2024-08-28 15:29:34 +0600

    use vite's enhanced:img instead of normal img tag for img handling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,7 +10,7 @@
 </svelte:head>
 
 <section class="welcome-container">
-	<img src={welcome} alt="Welcome" class="welcome-image" />
+	<enhanced:img src={welcome} alt="Welcome to PocketKit" class="welcome-image" />
 	<h1>Welcome to PocketKit <Iconify icon="smile" /></h1>
 	<p>A PocketBase x SvelteKit Starter Kit</p>
 	<Button href="https://github.com/hitochito/pocketkit/blob/main/README.md">Get Started</Button>
