47ea50384c8ea5a276a3fd7145917812d93395aa
hitochito <hitochito0@gmail.com>
2024-08-28 13:29:34 +0600

    add progress loader in layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,25 @@
-<script>
+<script lang="ts">
 	import '@fontsource-variable/grandstander'; // weights 100-900;
 	import { ModeWatcher } from 'mode-watcher';
 	import Header from './Header.svelte';
 	import Footer from './Footer.svelte';
 	import '../app.css';
+
+	// Progress Loader While Navigating Between Pages
+	import { navigating } from '$app/stores';
+	import '$lib/utils/nprogress/nprogress.css';
+	import NProgress from '$lib/utils/nprogress/nprogress';
+
+	NProgress.configure({
+		minimum: 0.56,
+		showSpinner: false
+	});
+
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		} else NProgress.done();
+	}
 </script>
 
 <ModeWatcher />
